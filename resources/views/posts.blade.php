8b9e0ea02223e7f4b925e184d0287b047dbc7f64
Alan Jack <60137518+aldojack@users.noreply.github.com>
2022-10-19 21:05:27 +0100

    Added routes for categories and can now filter by type of posts

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,6 +2,7 @@
     @foreach($posts as $post)
         <article class="mb-4">
             <h1 class="font-bold text-lg">{{$post->title}}</h1>
+            <a class="border-2 inline-block px-4 py-1 rounded-full text-sm" href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a>
             <p class="my-2">{{$post->excerpt}}</p>
             <a class= "bg-blue-500 ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-5" href="/posts/{{$post->slug}}">Read More </a>
         </article>
