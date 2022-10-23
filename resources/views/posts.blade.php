6857c662cc42c5ad259042dcab4aa57f2720251d
Alan Jack <60137518+aldojack@users.noreply.github.com>
2022-10-24 00:16:18 +0100

    Updated the routes and now can search on posts by specific author and the url is by username rather than id number

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,6 +2,7 @@
     @foreach($posts as $post)
         <article class="hover:bg-gray-300 mb-4 px-2 py-4">
             <h1 class="font-bold text-lg">{{$post->title}}</h1>
+            <p>By<a href="/authors/{{$post->author->username}}" class="text-blue-600 underline"> {{$post->author->name}}</a></p>
             <a class="border-2 inline-block px-4 py-1 rounded-full text-sm" href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a>
             <p class="my-2">{{$post->excerpt}}</p>
             <a class= "bg-blue-500 ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-5" href="/posts/{{$post->slug}}">Read More </a>
