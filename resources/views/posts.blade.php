83ce25caabab19c48cbeb4fab90913971cea1603
Alan Jack <60137518+aldojack@users.noreply.github.com>
2022-10-25 23:43:28 +0100

    Featured card now updated dynamically with props

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,18 +1,8 @@
 <x-layout>
     @include('_posts-header')
-{{--    @foreach($posts as $post)--}}
-{{--        <article class="hover:bg-gray-300 mb-4 px-2 py-4">--}}
-{{--            <h1 class="font-bold text-lg">{{$post->title}}</h1>--}}
-{{--            <p>By<a href="/authors/{{$post->author->username}}" class="text-blue-600 underline"> {{$post->author->name}}</a></p>--}}
-{{--            <a class="border-2 inline-block px-4 py-1 rounded-full text-sm" href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a>--}}
-{{--            <p class="my-2">{{$post->excerpt}}</p>--}}
-{{--            <a class= "bg-blue-500 ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-5" href="/posts/{{$post->slug}}">Read More </a>--}}
-{{--        </article>--}}
-{{--    @endforeach--}}
-
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-        <x-post-featured-card />
+        <x-post-featured-card :post="$posts[0]"/>
 
         <div class="lg:grid lg:grid-cols-2">
             <x-post-card />
