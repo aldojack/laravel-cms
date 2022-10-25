a0fcef9bc4044e7f81d5fd95ea2844f1cf839bec
Alan Jack <60137518+aldojack@users.noreply.github.com>
2022-10-26 00:34:00 +0100

    Refactored and created a posts grid component, also refactored to change grid size per posts limit

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,19 +2,10 @@
     @include('_posts-header')
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-        <x-post-featured-card :post="$posts[0]"/>
-
-        <div class="lg:grid lg:grid-cols-2">
-            @foreach($posts->skip(1) as $post)
-                <x-post-card :post="$post"/>
-            @endforeach
-        </div>
-
-        <div class="lg:grid lg:grid-cols-3">
-{{--            <x-post-card />--}}
-{{--            <x-post-card />--}}
-{{--            <x-post-card />--}}
-
-        </div>
+        @if($posts->count())
+            <x-post-grid :posts="$posts" />
+        @else
+            <p class="text-center">No blog posts yet please check back later!</p>
+        @endif
     </main>
 </x-layout>
