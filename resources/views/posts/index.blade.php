7837a69669494368cfa1d7842d03b60a60562751
Alan Jack <60137518+aldojack@users.noreply.github.com>
2022-10-30 19:00:18 +0000

    Video 40 - Extract category dropdown and compartmentalise the post blades

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,9 +1,9 @@
 <x-layout>
-    @include('_posts-header')
+    @include('posts._header')
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
         @if($posts->count())
-            <x-post-grid :posts="$posts" />
+            <x-post-grid :posts="$posts"/>
         @else
             <p class="text-center">No blog posts yet please check back later!</p>
         @endif
