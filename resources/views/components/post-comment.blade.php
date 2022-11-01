78014003620fabcc8fe3a3439617c70bbcec1b8f
Alan Jack <60137518+aldojack@users.noreply.github.com>
2022-11-01 23:43:54 +0000

    Comment model and factory added which now dynamically can add to posts

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,13 +1,22 @@
-<article class="flex bg-gray-200 p-6 roundex-xl border border-gray-300 space-x-4">
+@props(['comment'])
+
+<article class="flex bg-gray-100 border border-gray-200 p-6 rounded-xl space-x-4">
     <div class="flex-shrink-0">
-        <img src="https://i.pravatar.cc/60" alt="" class="rounded-xl">
+        <img src="https://i.pravatar.cc/60?u={{ $comment->id }}" alt="" width="60" height="60" class="rounded-xl">
     </div>
 
     <div>
         <header class="mb-4">
-            <h3 class="font-bold">John Doe</h3>
-            <p class="text-xs">Posted <time>8 months ago</time></p>
+            <h3 class="font-bold">{{ $comment->author->username }}</h3>
+
+            <p class="text-xs">
+                Posted
+                <time>{{ $comment->created_at }}</time>
+            </p>
         </header>
-        <p>Hate dogs cuddle no cuddle cuddle love scratch scratch get poop stuck in paws jumping out of litter box and run around the house scream meowing and smearing hot cat mud all over bite the neighbor's bratty kid. </p>
+
+        <p>
+            {{ $comment->body }}
+        </p>
     </div>
 </article>
