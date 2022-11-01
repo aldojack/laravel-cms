1affdb7494f995643798ebf696ebefb61e65c559
Aldo <60137518+aldojack@users.noreply.github.com>
2022-11-01 11:08:11 +0000

    Added basic Register page with form that validates and updates database

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    public function create(){
+        return view('register.create');
+    }
+
+    public function store(){
+
+        $attributes = request()->validate([
+            'name'=> ['required','max:50'],
+            'username'=>['required','max:50','min:5'],
+            'email' => ['required','email','max:50'],
+            'password' => ['required','max:50','min:8']
+        ]);
+        User::create($attributes);
+
+        return redirect('/');
+    }
+}
