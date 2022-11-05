1cb5ac1e4bb8722acdc46f514b9a958f698a22f9
Alan Jack <60137518+aldojack@users.noreply.github.com>
2022-11-05 12:59:33 +0000

    Extracted Newsletter to a class and refactored the routes to be inline with rest of current style

--- routes/web.php
+++ routes/web.php
@@ -4,35 +4,19 @@ use App\Http\Controllers\PostCommentsController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
+use App\Http\Controllers\NewsletterController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Validation\ValidationException;
-use MailchimpMarketing\ApiClient;
+use App\Services\Newsletter;
 
 
-Route::post('newsletter', function(){
-    request()->validate(['email' => 'required|email']);
-    $mailchimp = new ApiClient();
 
-    $mailchimp->setConfig([
-        'apiKey' => config('services.mailchimp.key'),
-        'server' => 'us12'
-    ]);
-    try{
-        $response = $mailchimp->lists->addListMember('a06547c296', [
-            'email_address' => request('email'),
-            'status' => 'subscribed'
-        ]);
-    } catch(\Exception $e){
-        throw ValidationException::withMessages([
-            'email' => 'Invalid email, could not be added to our newsletter list.'
-        ]);
-    };
-    return redirect('/')->with('success','You are signed up to our newsletter');
-});
 
 Route::get('/', [PostController::class,  'index'])->name('home');
 Route::get('/posts/{post:slug}', [PostController::class, 'show']);
 Route::post('/posts/{post:slug}/comments',[PostCommentsController::class,'store']);
+
+Route::post('newsletter',NewsletterController::class);
 
 Route::get('register',[RegisterController::class, 'create'])->middleware('guest');
 Route::post('register',[RegisterController::class, 'store'])->middleware('guest');
