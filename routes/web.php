fe9c33b77eebf9d9f1e0ad07a523a9128b347193
Alan Jack <60137518+aldojack@users.noreply.github.com>
2022-11-05 11:39:12 +0000

    Imported mailchip and configured with laravel

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,21 @@ use App\Http\Controllers\SessionsController;
 use Illuminate\Support\Facades\Route;
 
 
+Route::get('ping', function(){
+    $mailchimp = new \MailchimpMarketing\ApiClient();
+
+    $mailchimp->setConfig([
+        'apiKey' => config('services.mailchimp.key'),
+        'server' => 'us12'
+    ]);
+
+    $response = $mailchimp->lists->addListMember('a06547c296', [
+        'email_address' => "b00419166@studentmail.uws.ac.uk",
+        'status' => 'subscribed'
+    ]);
+    ddd($response);
+});
+
 Route::get('/', [PostController::class,  'index'])->name('home');
 Route::get('/posts/{post:slug}', [PostController::class, 'show']);
 Route::post('/posts/{post:slug}/comments',[PostCommentsController::class,'store']);
