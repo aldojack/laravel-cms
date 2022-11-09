3ff97149fa3d8af84ad1ced20e70fa420c9c58eb
Alan Jack <60137518+aldojack@users.noreply.github.com>
2022-11-09 11:37:55 +0000

    Now have edit, delete end points.  AdminPostController for all admin controls

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -27,15 +27,10 @@
                         <x-slot name="trigger">
                             <button class="text-xs font-bold uppercase">Welcome, {{auth()->user()->name}}</button>
                         </x-slot>
-                        <x-dropdown-item href="/" :active="request()->is('/')">
-                            Home
-                        </x-dropdown-item>
-                        <x-dropdown-item href="/admin/posts/create" :active="request()->is('admin/posts/create')">
-                            New Post
-                        </x-dropdown-item>
-                        <x-dropdown-item href="#" x-data="{}" @click.prevent="document.querySelector('#logout-form').submit()">
-                            Logout
-                        </x-dropdown-item>
+                        <x-dropdown-item href="/" :active="request()->is('/')">Home</x-dropdown-item>
+                        <x-dropdown-item href="/admin/posts/" :active="request()->is('admin/posts/')">Dashboard</x-dropdown-item>
+                        <x-dropdown-item href="/admin/posts/create" :active="request()->is('admin/posts/create')">New Post</x-dropdown-item>
+                        <x-dropdown-item href="#" x-data="{}" @click.prevent="document.querySelector('#logout-form').submit()">Logout</x-dropdown-item>
 
                         <form method="post" action="/logout" class="hidden" id="logout-form">
                             @csrf
