84027cc40458cec210a3d6fceced6126aec9c6d5
Aldo <60137518+aldojack@users.noreply.github.com>
2022-11-15 12:07:33 +0000

    Fixed issue with admin posts

--- app/Http/Controllers/AdminPostController.php
+++ app/Http/Controllers/AdminPostController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
-use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
 
 class AdminPostController extends Controller
@@ -23,8 +22,8 @@ class AdminPostController extends Controller
     public function store()
     {
         Post::create(array_merge($this->validatePost(), [
-           'user_id' => request()->user()->id(),
-           'thumbnail' => request()->file('thumbnail')->store('thumbnails')
+            'user_id' => request()->user()->id,
+            'thumbnail' => request()->file('thumbnail')->store('thumbnails')
         ]));
 
         return redirect('/');
@@ -32,36 +31,29 @@ class AdminPostController extends Controller
 
     public function edit(Post $post)
     {
-        return view('admin.posts.edit', [
-            'post' => $post
-        ]);
+        return view('admin.posts.edit', ['post' => $post]);
     }
 
     public function update(Post $post)
     {
         $attributes = $this->validatePost($post);
 
-        if($attributes['thumbnail'] ?? false)
-        {
+        if ($attributes['thumbnail'] ?? false) {
             $attributes['thumbnail'] = request()->file('thumbnail')->store('thumbnails');
         }
 
         $post->update($attributes);
 
-        return back()->with('success', 'Post updated');
+        return back()->with('success', 'Post Updated!');
     }
 
     public function destroy(Post $post)
     {
         $post->delete();
 
-        return back()->with('success', 'Post deleted');
+        return back()->with('success', 'Post Deleted!');
     }
 
-    /**
-     * @param Post $post
-     * @return array
-     */
     protected function validatePost(?Post $post = null): array
     {
         $post ??= new Post();
