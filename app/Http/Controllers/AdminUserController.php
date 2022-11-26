ff02b85bea3d1a0c1aa2dd0546d5d388a80aebed
Alan Jack <aldojack@hotmail.co.uk>
2022-11-26 14:48:00 +0000

    Update removal of admin function and removed commented out code

--- app/Http/Controllers/AdminUserController.php
+++ app/Http/Controllers/AdminUserController.php
@@ -17,42 +17,9 @@ class AdminUserController extends Controller
         ]);
     }
 
-    // public function create()
-    // {
-    //     return view('admin.users.create');
-    // }
-
-    // public function store()
-    // {
-    //     Post::create(array_merge($this->validatePost(), [
-    //         'user_id' => request()->user()->id,
-    //         'thumbnail' => request()->file('thumbnail')->store('/images/thumbnails')
-    //     ]));
-
-    //     return redirect('/');
-    // }
-
-    // public function edit(User $user)
-    // {
-    //     return view('admin.users.edit', ['user' => $user]);
-    // }
-
-    // public function update(User $user)
-    // {
-    //     $attributes = $this->validatePost($user);
-
-    //     if ($attributes['thumbnail'] ?? false) {
-    //         $attributes['thumbnail'] = request()->file('thumbnail')->store('thumbnails');
-    //     }
-
-    //     $post->update($attributes);
-
-    //     return back()->with('success', 'Post Updated!');
-    // }
-
     public function destroy(User $user)
     {
-            if(request()->user()->id !== $user->id)
+            if($user->isAdmin === 0)
             {
                 $user->posts->each(function ($post) {
                     if(Storage::disk('public')->exists($post->thumbnail))
@@ -69,22 +36,7 @@ class AdminUserController extends Controller
 
             else 
             {
-            return back()->with('error', 'Unable to delete your own account');
+            return back()->with('error', 'Unable to delete admin accounts');
             }
     }
-
-
-    // protected function validatePost(?Post $post = null): array
-    // {
-    //     $post ??= new Post();
-
-    //     return request()->validate([
-    //         'title' => 'required',
-    //         'thumbnail' => $post->exists ? ['image'] : ['required', 'image'],
-    //         'slug' => ['required', Rule::unique('posts', 'slug')->ignore($post)],
-    //         'excerpt' => 'required',
-    //         'body' => 'required',
-    //         'category_id' => ['required', Rule::exists('categories', 'id')]
-    //     ]);
-    // }
 }
